d4a70048a66c287a739f947ef885d5b97cb66b96
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-07-26 21:19:33 +0530

    [Samriddhi] Fix. OOPs logic in the direction distance class

--- src/test/java/org/oop/DistanceAndDirectionCalculatorTest.java
+++ src/test/java/org/oop/DistanceAndDirectionCalculatorTest.java
@@ -3,14 +3,15 @@ package org.oop;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.oop.DistanceAndDirectionCalculator.direction;
 
 public class DistanceAndDirectionCalculatorTest {
 
     @Test
     void twoPointsWithSameXAndYCoordinatesShouldHaveADistanceOfZero() {
         Point origin = new Point(0, 0);
-        Assertions.assertEquals(0, DistanceAndDirectionCalculator.distance(origin, origin));
+        DistanceAndDirectionCalculator distanceAndDirectionCalculator = new DistanceAndDirectionCalculator();
+
+        Assertions.assertEquals(0, distanceAndDirectionCalculator.distance(origin, origin));
     }
 
     @Test
@@ -18,17 +19,19 @@ public class DistanceAndDirectionCalculatorTest {
         Point origin = new Point(0, 0);
         Point point1 = new Point(1, 0);
         Point point2 = new Point(0, 1);
+        DistanceAndDirectionCalculator distanceAndDirectionCalculator = new DistanceAndDirectionCalculator();
 
-        Assertions.assertEquals(1, DistanceAndDirectionCalculator.distance(origin, point1));
-        Assertions.assertEquals(1, DistanceAndDirectionCalculator.distance(origin, point2));
+        Assertions.assertEquals(1, distanceAndDirectionCalculator.distance(origin, point1));
+        Assertions.assertEquals(1, distanceAndDirectionCalculator.distance(origin, point2));
     }
 
     @Test
     void distanceBetweenTwoOppositePointsOnUnitCircleShouldBeTwo() {
         Point point1 = new Point(1, 0);
         Point point2 = new Point(-1, 0);
+        DistanceAndDirectionCalculator distanceAndDirectionCalculator = new DistanceAndDirectionCalculator();
 
-        Assertions.assertEquals(2, DistanceAndDirectionCalculator.distance(point1, point2));
+        Assertions.assertEquals(2, distanceAndDirectionCalculator.distance(point1, point2));
     }
 
     @Test
@@ -36,9 +39,10 @@ public class DistanceAndDirectionCalculatorTest {
         Point origin = new Point(0, 0);
         Point point1 = new Point(1, 0);
         Point point2 = new Point(3, 0);
+        DistanceAndDirectionCalculator distanceAndDirectionCalculator = new DistanceAndDirectionCalculator();
 
-        Assertions.assertEquals(0, direction(origin, point1));
-        Assertions.assertEquals(0, direction(origin, point2));
+        Assertions.assertEquals(0, distanceAndDirectionCalculator.direction(origin, point1));
+        Assertions.assertEquals(0, distanceAndDirectionCalculator.direction(origin, point2));
     }
 
     @Test
@@ -46,9 +50,10 @@ public class DistanceAndDirectionCalculatorTest {
         Point origin = new Point(0, 0);
         Point point1 = new Point(-1, 0);
         Point point2 = new Point(-3, 0);
+        DistanceAndDirectionCalculator distanceAndDirectionCalculator = new DistanceAndDirectionCalculator();
 
-        Assertions.assertEquals(Math.PI, direction(origin, point1));
-        Assertions.assertEquals(Math.PI, direction(origin, point2));
+        Assertions.assertEquals(Math.PI, distanceAndDirectionCalculator.direction(origin, point1));
+        Assertions.assertEquals(Math.PI, distanceAndDirectionCalculator.direction(origin, point2));
     }
 
     @Test
@@ -56,8 +61,9 @@ public class DistanceAndDirectionCalculatorTest {
         Point origin = new Point(0, 0);
         Point point1 = new Point(0, 1);
         Point point2 = new Point(0, 3);
+        DistanceAndDirectionCalculator distanceAndDirectionCalculator = new DistanceAndDirectionCalculator();
 
-        Assertions.assertEquals(Math.PI / 2, direction(origin, point2));
-        Assertions.assertEquals(Math.PI / 2, direction(origin, point1));
+        Assertions.assertEquals(Math.PI / 2, distanceAndDirectionCalculator.direction(origin, point2));
+        Assertions.assertEquals(Math.PI / 2, distanceAndDirectionCalculator.direction(origin, point1));
     }
 }
